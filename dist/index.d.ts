1f7b5a7d36a35d20241d5cbecc2b1aef7ecc90c7
Elitezen <61894124+Elitezen@users.noreply.github.com>
2022-03-15 14:56:26 -0400

    2.0.0 Initial Source Commit (2)

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import getQuestions from "./src/Functions/Public/getQuestions";
-import Category from "./src/Classes/Category";
-import Validator from "./src/Classes/Validator";
-import { EasyTriviaError, OpenTriviaDBError } from "./src/Classes/Errors";
-export { Category, EasyTriviaError, getQuestions, OpenTriviaDBError, Validator };
