43428cbe941a538c2ec815af799d48c7cc9a5eb7
Richard Li <richprli@amazon.com>
2023-03-16 13:01:41 -0700

    updated gitignore and removed dist/

--- dist/src/classes/OpenTDBError.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ErrorResponse } from "../typings/interfaces";
-/**
- * @class OpenTDB error constructor
- * @private
- */
-export default class OpenTDBError extends TypeError {
-    constructor(error: ErrorResponse);
-}
